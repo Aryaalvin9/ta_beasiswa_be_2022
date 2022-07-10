d3bd2ed1359c8f2b408e4dab529d793f0ef7df6f
Aryaalvin9 <aryaalvin9@gmail.com>
2022-07-10 14:03:29 +0700

    update

--- koneksi.js
+++ koneksi.js
@@ -394,7 +394,7 @@ const addDataBanding = (id,fakultas, penghasilan, gaji)=>{
 const updateDataBanding = (request, response) => {
     var{id_user, nilai_rata, tingkat_sertif, fakultas, jumlah_sertifikat, jenis_sertifikat} = request.body
 
-    var point_sertifikat = jumlah_sertifikat * jenis_sertifikat;
+    var point_sertifikat = Math.ceil(jumlah_sertifikat * jenis_sertifikat);
     pool.query('update tbl_banding set niali_rata = $2, tingkat_sertif  = $3, jumlah_sertifikat = $4, jenis_sertifikat = $5, point_sertifikat = $6  where  id_user = $1',[id_user, nilai_rata,tingkat_sertif, jumlah_sertifikat, jenis_sertifikat, point_sertifikat],(error, results)=>{
         if(error){
             throw error
